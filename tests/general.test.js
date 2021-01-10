97407f61ba40fa28003caba9eec7d12ed9793324
dhanuprys <dhanuprys@gmail.com>
2021-01-10 19:40:35 +0800

    Write new test

--- tests/general.test.js
+++ tests/general.test.js
@@ -1,4 +1,6 @@
 const { BrainlyAPI, Server } = require('../build');
+const { default: AnonymousWorker } = require('../build/workers/AnonymousWorker');
+const { default: AuthWorker } = require('../build/workers/AuthWorker');
 
 test('should return current version of the library', () => {
     const { version: currentVersion } = require('../package.json');
@@ -11,4 +13,18 @@ test('should return correct server information', () => {
 
     expect(server.Code).toBe('ID');
     expect(server.Name).toBe('Indonesia');
+});
+
+describe('calling application starter method', () => {
+    test('without auth and without config', async () => {
+        await BrainlyAPI.startWorker(brainly => {
+            expect(brainly).toBeInstanceOf(AnonymousWorker);
+        });
+    });
+
+    test('with auth', async () => {
+        await BrainlyAPI.startWorker({ auth: true }, auth => {
+            expect(auth).toBeInstanceOf(AuthWorker);
+        });
+    });
 });
