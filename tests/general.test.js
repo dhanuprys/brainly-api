1583d1f3c938768e939574e35cb65a5ad1576fad
dhanuprys <dhanuprys@gmail.com>
2021-01-10 14:38:47 +0800

    Create new localization test

--- tests/general.test.js
+++ tests/general.test.js
@@ -1,7 +1,14 @@
-const { BrainlyAPI } = require('../build');
+const { BrainlyAPI, Server } = require('../build');
 
 test('should return current version of the library', () => {
     const { version: currentVersion } = require('../package.json');
     
     expect(BrainlyAPI.VERSION).toEqual(currentVersion);
+});
+
+test('should return correct server information', () => {
+    const server = Server.ID;
+
+    expect(server.Code).toBe('ID');
+    expect(server.Name).toBe('Indonesia');
 });
