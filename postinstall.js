f590e1d43bcefa6b7ceadf2f390562dd225ab50c
dhanuprys <dhanuprys@gmail.com>
2021-01-11 21:01:57 +0800

    Update postinstall program

--- postinstall.js
+++ postinstall.js
@@ -1,3 +1,11 @@
+const { env } = require('process');
+const path = require('path');
 const chalk = require('chalk');
+const rimraf = require('rimraf');
+
+if (env.BUILD_MODE === '1') {
+    console.log(chalk.blueBright('Removing unused files..'));
+    rimraf(path.resolve(__dirname, 'src'), () => {});
+}
 
 console.log(chalk.yellowBright('Thanks for using brainly-api :)'));
