aa104bdcdd6a1b8a141cbc85ff9d6087d58da587
dhanuprys <dhanuprys@gmail.com>
2021-01-11 20:15:15 +0800

    Write new encryption utilities

---
+++ src/utils/Encryption.ts
@@ -0,0 +1,17 @@
+import Cryptr from 'cryptr';
+
+class Encryption {
+    private static ENCRYPT_PASSWORD: string = 'D7D4C5B8D2B5968184AEF19A6E8D3';
+
+    private static _encryption: Cryptr = new Cryptr(Encryption.ENCRYPT_PASSWORD);
+
+    static decrypt(text: string): string {
+        return Encryption._encryption.decrypt(text);
+    }
+
+    static encrypt(text: string): string {
+        return Encryption._encryption.encrypt(text);
+    }
+}
+
+export default Encryption;
