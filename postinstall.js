18ca6b308c84d5781e8fbd138582d62b40dec090
dhanuprys <dhanuprys@gmail.com>
2021-01-12 11:11:26 +0800

    Add script for remove tsconfig.json

--- postinstall.js
+++ postinstall.js
@@ -5,7 +5,9 @@ const rimraf = require('rimraf');
 
 if (env.BUILD_MODE === '1') {
     console.log(chalk.blueBright('Removing unused files..'));
-    rimraf(path.resolve(__dirname, 'src'), () => {});
+    rimraf(path.resolve(__dirname, 'src'), () => {
+        rimraf(path.resolve(__dirname, 'tsconfig.json'), () => {});
+    });
 }
 
 console.log(chalk.yellowBright('Thanks for using brainly-api :)'));
