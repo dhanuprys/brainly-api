c9803da96181e2a85797fada0075edcbf7d880cf
dhanuprys <dhanuprys@gmail.com>
2021-01-12 11:31:05 +0800

    Add method comment

--- src/BrainlyAPI.ts
+++ src/BrainlyAPI.ts
@@ -6,15 +6,22 @@ import AuthWorker from './workers/AuthWorker';
 class BrainlyAPI {
     static VERSION = require('./../package.json').version;
 
+    /**
+     * Start worker adalah metode yang digunakan sebagai pembungkus
+     * pekerjaan
+     */
     static async startWorker(
         config: Configurable.BrainlyWorker | Configurable.StarterConfig,
         worker?: Configurable.BrainlyWorker
     ): Promise<Promise<any>> {
+        // Konfigurasi-konfigurasi default
         const defaultConfig: Configurable.StarterConfig = {
             auth: false,
             server: Server.US
         };
 
+        // Jika parameter pertama "config", diisi dengan data bertipe "function"
+        // maka program akan menganggapnya tidak memiliki konfigurasi
         if (typeof config === 'function') {
             worker = config as Configurable.BrainlyWorker;
 
@@ -29,17 +36,23 @@ class BrainlyAPI {
         if (typeof config === 'object' && typeof worker === 'function') {
             config = Object.assign(defaultConfig, config);
 
+            // Jika parameter konfigurasi memiliki properti "auth" maka fungsi callback
+            // akan dipanggil, kemudian parameter pertama akan diisi dengan objek dari kelas
+            // AuthWorker
             if (config.auth === true) {
                 return await worker(
                     new AuthWorker(config.server)
                 );
             }
             
+            // Jika tidak kondisi yang tepat dari logika di atas maka program akan memanggil
+            // fungsi callback dan melawatkan parameter-parameter default
             return await worker(
                 new AnonymousWorker(config.server)
             );
         }
 
+        // Default return
         return null;
     }
 }
