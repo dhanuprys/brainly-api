63471d0b0585226a4d8216774fdd7e780cb0c7cb
dhanuprys <dhanuprys@gmail.com>
2021-01-13 18:55:45 +0800

    Re configuring package manifest

---
+++ cli.js
@@ -0,0 +1,19 @@
+const process = require('process');
+const chalk = require('chalk');
+const npm = require('npm');
+
+console.log(chalk.bold('Loading npm resources...'));
+npm.load({}, (error, npm) => {
+    if (error) {
+        console.log(chalk.bold.redBright('Init failed'));
+        return;
+    }
+
+    console.log(chalk.bold.yellowBright('Installing puppeteer...'));
+
+    npm.commands.install(['puppeteer'], () => {
+        npm.commands.install(['--save-dev', '@types/puppeteer'], () => {
+            console.log(chalk.bold.blueBright('Done...'));
+        });
+    });
+});
