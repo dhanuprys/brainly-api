d320d0fe4703966c2870b294ce4657a169234bce
dhanuprys <dhanuprys@gmail.com>
2021-01-16 19:00:40 +0800

    Write new client request

---
+++ src/clients/AnonymousHttp.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import Configurable from '../configurable';
+import ServerInformation from '../contracts/ServerInformation';
+
+class AnonymousHttp {
+    private _serverInformation: ServerInformation;
+
+    constructor(server: ServerInformation) {
+        this._serverInformation = server;
+    }
+
+    requestCommonApi<T extends keyof Configurable.ApiPath.Anonymous>(apiType: T, ...args: any[]) {
+        let action;
+
+        try {
+            // Memuat library secara dinamis
+            action = require('./actions/' + apiType)
+        } catch (error) {
+            return false;
+        }
+
+        return action(this._serverInformation, ...args);
+    }
+}
+
+export default AnonymousHttp;
