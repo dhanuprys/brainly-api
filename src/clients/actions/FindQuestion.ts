772050893c1501abec6e55bc709d7cd3f6e2f62f
dhanuprys <dhanuprys@gmail.com>
2021-01-17 11:26:17 +0800

    Add return value

--- src/clients/actions/FindQuestion.ts
+++ src/clients/actions/FindQuestion.ts
@@ -1,7 +1,8 @@
 import ServerInformation from '../../contracts/ServerInformation';
+import QuestionResults from '../../wrappers/QuestionResults';
 
 async function FindQuestion(server: ServerInformation, config: { question: string }) {
-    
+    return new QuestionResults();
 }
 
 export default FindQuestion;
--- src/clients/actions/GetQuestionHighlight.ts
+++ src/clients/actions/GetQuestionHighlight.ts
@@ -1,8 +1,9 @@
 import ServerInformation from '../../contracts/ServerInformation';
 import Configurable from '../../configurable';
+import QuestionResults from '../../wrappers/QuestionResults';
 
 async function GetQuestionHighlight(server: ServerInformation, config: { lesson: Configurable.LessonItem }) {
-
+    return new QuestionResults();
 }
 
 export default GetQuestionHighlight;
