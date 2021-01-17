e948653f5643e2fdcb261e6d8f492913f6679ea8
dhanuprys <dhanuprys@gmail.com>
2021-01-17 11:23:45 +0800

    Write new action

---
+++ src/clients/actions/GetQuestionHighlight.ts
@@ -0,0 +1,8 @@
+import ServerInformation from '../../contracts/ServerInformation';
+import Configurable from '../../configurable';
+
+async function GetQuestionHighlight(server: ServerInformation, config: { lesson: Configurable.LessonItem }) {
+
+}
+
+export default GetQuestionHighlight;
