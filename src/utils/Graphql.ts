221fd0b082b1fd4ee20428a6d9233ca3e0a05d4f
dhanuprys <dhanuprys@gmail.com>
2021-01-18 13:23:40 +0800

    Wrap return value with an array

--- src/utils/Graphql.ts
+++ src/utils/Graphql.ts
@@ -1,11 +1,11 @@
 // static class
 class Graphql {
-    static generate(operationName: string, variables: any, query: any): any {
-        return {
+    static generate(operationName: string, variables: any, query: any): any[] {
+        return [{
             operationName: operationName,
             variables: variables,
             query: query
-        };
+        }];
     }
 }
 
