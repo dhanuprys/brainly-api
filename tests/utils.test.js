a7b911824721fc5efaab2ef5409d9ae47660ac77
dhanuprys <dhanuprys@gmail.com>
2021-01-18 12:55:35 +0800

    Create Graphql utils tester

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,9 +1,22 @@
 const { default: Encryption } = require('../build/utils/Encryption');
+const { default: Graphql } = require('../build/utils/Graphql');
 
 describe('test encryption utils', () => {
     test('decrypting a text', () => {
         expect(
             Encryption.decrypt(Encryption.encrypt('foobar'))
         ).toBe('foobar');
+    });
+});
+
+describe('test graphql query generator', () => {
+    test('should return expected properties', () => {
+        const graphqlRawData = Graphql.generate('foo', { bar: true }, { beer: false });
+
+        expect(graphqlRawData).toEqual({ 
+            operationName: 'foo', 
+            variables: { bar: true },
+            query: { beer: false }
+        });
     });
 });
