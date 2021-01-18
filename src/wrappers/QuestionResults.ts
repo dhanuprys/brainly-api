42ba33bb07c16fff3f3e118028c2f3e636935ae6
dhanuprys <dhanuprys@gmail.com>
2021-01-18 13:45:39 +0800

    Add handler

--- src/wrappers/QuestionResults.ts
+++ src/wrappers/QuestionResults.ts
@@ -4,25 +4,41 @@ import QuestionDetail from './QuestionDetail';
 
 class QuestionResults implements WithIterator<QuestionDetail>, WithRawData {
     readonly raw: any;
+    private _questionDetails: QuestionDetail[];
 
     constructor(raw: any = {}) {
         this.raw = raw;
+        this._questionDetails = [];
+
+        for (const question of this.raw[0].data.questionSearch.edges) {
+            this._questionDetails.push(
+                new QuestionDetail(question)
+            );
+        }
     }
 
     async getFirstQuestion(): Promise<QuestionDetail | null> {
-        return new QuestionDetail();
+        return this.raw[0].data.questionSearch.count < 1 
+            ? null 
+            : this._questionDetails[0];
     }
 
-    async getAllQuestion(): Promise<QuestionDetail[] | null> {
-        return [new QuestionDetail()];
+    async getAllQuestion(): Promise<QuestionDetail[]> {
+        return [...this._questionDetails];
     }
 
     async getLastQuestion(): Promise<QuestionDetail | null> {
-        return new QuestionDetail();
+        return this.raw[0].data.questionSearch.count < 1 
+            ? null 
+            : new QuestionDetail(this._questionDetails[
+                this._questionDetails.length-1
+            ]);
     }
 
     *[Symbol.iterator](): Iterator<QuestionDetail> {
-        yield new QuestionDetail();
+        for (const question of this._questionDetails) {
+            yield question;
+        }
     }
 }
 
