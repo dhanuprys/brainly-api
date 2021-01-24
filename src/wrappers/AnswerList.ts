1e102bb37bf07eb963d91d8f5cda0e014b7b8efa
dhanuprys <dhanuprys@gmail.com>
2021-01-24 11:56:22 +0800

    Modifying output

--- src/wrappers/AnswerList.ts
+++ src/wrappers/AnswerList.ts
@@ -1,27 +1,40 @@
 import WithIterator from '../contracts/WithIterator';
+import WithRawData from '../contracts/WithRawData';
 import AnswerDetail from './AnswerDetail';
 
-class AnswerList implements WithIterator<AnswerDetail> {
-    private readonly _database: any;
+class AnswerList implements WithIterator<AnswerDetail>, WithRawData {
+    readonly raw: any;
+    private _answerDetails: AnswerDetail[];
 
-    constructor(database: any = {}) {
-        this._database = database;
+    constructor(raw: any = {}) {
+        this.raw = raw;
+        this._answerDetails = [];
+
+        for (const answer of this.raw.nodes) {
+            this._answerDetails.push(answer);
+        }
     }
 
-    async getFirstAnswer() {
-
+    async getFirstAnswer(): Promise<AnswerDetail | null> {
+        return this._answerDetails.length > 0
+            ? this._answerDetails[0]
+            : null;
     }
 
-    async getLastAnswer() {
-
+    async getLastAnswer(): Promise<AnswerDetail | null> {
+        return this._answerDetails.length > 0
+            ? this._answerDetails[this._answerDetails.length-1]
+            : null;
     }
 
     async getAllAnswer(): Promise<AnswerDetail[]> {
-        return [new AnswerDetail()];
+        return [...this._answerDetails];
     }
 
     *[Symbol.iterator](): Iterator<AnswerDetail> {
-
+        for (const answer of this._answerDetails) {
+            yield answer;
+        }
     }
 }
 
