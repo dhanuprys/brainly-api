6713a8fbd0681f33086a9e014939a4b097915150
dhanuprys <dhanuprys@gmail.com>
2021-01-25 11:32:25 +0800

    Rewrite all commands

--- cli.js
+++ cli.js
@@ -1,57 +1,56 @@
 #!/usr/bin/env node
 
-const process = require('process');
+const fs = require('fs');
+const path = require('path');
+const { program } = require('commander');
+const highlighter = require('cli-highlight');
 const chalk = require('chalk');
 const npm = require('npm');
-const { BrainlyAPI } = require('./build/index');
 
-const additionalPackage = [
-    ['puppeteer'],
-    ['--save-dev', '@types/puppeteer']
-];
+const EXAMPLE_PATH = './examples';
 
-/**
- * @param {string[]} packageList
- */
-function installPackage(packageList, npm) {
-    const currentPackage = packageList[0];
+program
+    .name('brainly-api')
+    .version('0.0.1')
+    .addHelpText('beforeAll', chalk.bold(`${chalk.greenBright('BRAINLY-API')} CLI TOOL`));
 
-    if (currentPackage === undefined) {
-        console.log(chalk.blueBright('Done.'));
-        return;
-    }
+program
+    .command('example-list')
+    .description('show examples list')
+    .action(() => {
+        const fileList = fs.readdirSync(EXAMPLE_PATH);
 
-    console.log('Installing ' + chalk.yellowBright(currentPackage[currentPackage.length - 1]) + '...');
-
-    npm.commands.install(currentPackage, () => {
-        installPackage(packageList.shift(), npm);
-    });
-}
-
-function showUp() {
-    console.log(chalk.bold.greenBright('BRAINLY-API CLI TOOLS'));
-    console.log(chalk.bold.redBright('THIS FEATURE IS STILL UNDER DEVELOPMENT'));
-
-    if (process.argv.length > 2) {
-        if (process.argv[2] === '--select') {
-            console.log('Please select package. (use \',\' to install multiply package)');
-            for (let i = 0; i < additionalPackage.length; i++) {
-                console.log(String(i) + ': ' + additionalPackage[i][additionalPackage[i].length - 1]);
-            }
-        } else if (process.argv[2] === '-v' || process.argv[2] === '--version') {
-            console.log(chalk.bold(BrainlyAPI.VERSION));
+        for (let i = 0; i < fileList.length; i++) {
+            const fileName = fileList[i];
+            console.log(String(i) + ': ' + fileName);
         }
-    } else {
-        console.log(chalk.bold('Loading npm resources...'));
-        npm.load({}, (error, npm) => {
-            if (error) {
-                console.log(chalk.bold.redBright('Init failed'));
-                return;
-            }
-            installPackage(additionalPackage, npm);
-        });
-    }
+    });
 
-}
+program
+    .command('example [index]')
+    .description('show example code')
+    .action(index => {
+        const fileList = fs.readdirSync(EXAMPLE_PATH);
+        let fileName = fileList[Number(index)];
 
-showUp();
+        if (index === undefined) {
+            console.log('Please run `brainly-api example-list`');
+            return;
+        }
+
+        if (fileName === undefined) {
+            console.log('Example not found');
+            return;
+        }
+
+        fileName = path.join(EXAMPLE_PATH, fileName);
+
+        console.log(
+            highlighter.highlight(
+                fs.readFileSync(fileName).toString(),
+                { languange: 'js' }
+            )
+        );
+    });
+
+program.parse();
