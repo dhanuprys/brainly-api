a50d133ce50764391812f7d20e5dac995b5643bc
dhanuprys <dhanuprys@gmail.com>
2021-01-28 20:52:46 +0800

    Fix bug

--- src/wrappers/UserProfile.ts
+++ src/wrappers/UserProfile.ts
@@ -2,21 +2,23 @@ import WithRawData from '../contracts/WithRawData';
 
 class UserProfile implements WithRawData {
     readonly raw: any;
+    private _profile: any;
 
     constructor(raw: any = {}) {
         this.raw = raw;
+        this._profile = this.raw[0].data;
     }
 
     async getId(): Promise<string> {
-        return <string>this.raw.userById.databaseId;
+        return <string>this._profile.userById.databaseId;
     }
 
     async getAvatar(): Promise<string> {
-        return <string>this.raw.userById.avatar.thumbnailUrl;
+        return <string>this._profile.userById.avatar.thumbnailUrl;
     }
 
     async getNick(): Promise<string> {
-        return <string>this.raw.userById.nick;
+        return <string>this._profile.userById.nick;
     }
 }
 
