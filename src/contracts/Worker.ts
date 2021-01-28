32c9c351cdde340a63291695e8fb94f1b2cc4b32
dhanuprys <dhanuprys@gmail.com>
2021-01-28 21:00:42 +0800

    Add new part of worker interface

--- src/contracts/Worker.ts
+++ src/contracts/Worker.ts
@@ -1,5 +1,6 @@
 import QuestionResults from '../wrappers/QuestionResults';
 import QuestionDetail from '../wrappers/QuestionDetail';
+import UserProfile from '../wrappers/UserProfile';
 import Configurable from '../configurable';
 
 interface Worker {
@@ -7,6 +8,7 @@ interface Worker {
     findQuestion(question: string): Promise<QuestionResults>;
     findQuestionHighlight(category: Configurable.LessonItem): Promise<QuestionResults>;
     findQuestionById(id: string): Promise<QuestionDetail>;
+    fetchUserProfile(userId: string): Promise<UserProfile>;
 }
 
 export default Worker;
