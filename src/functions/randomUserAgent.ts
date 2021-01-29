4f51f3ca5c5c95d425ecc0040f61afef3e804fcc
dhanuprys <dhanuprys@gmail.com>
2021-01-29 19:41:44 +0800

    Rebase

--- src/functions/randomUserAgent.ts
+++ src/functions/randomUserAgent.ts
@@ -1,12 +1,11 @@
+import safeRequire from './safeRequire';
+
 function randomUserAgent(): string {
     let userAgent: string = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1';
+    const randomUserAgent: any = safeRequire<any>('random-useragent');
 
-    try {
-        userAgent = require('random-useragent').getRandom();
-    } catch (loadError) {
-        if (loadError.code !== 'MODULE_NOT_FOUND') {
-            throw loadError;
-        }
+    if (randomUserAgent !== null) {
+        userAgent = randomUserAgent.getRandom();
     }
 
     return userAgent;
