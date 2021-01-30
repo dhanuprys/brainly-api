c86b2a16dfdcc38b0ce6613c498cd7a5de0e71c5
dhanuprys <dhanuprys@gmail.com>
2021-01-30 12:46:04 +0800

    Restructuring cli

---
+++ src/cli.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import { program } from 'commander';
+import highlighter = require('cli-highlight');
+import chalk from 'chalk';
+import safeRequire from './functions/safeRequire';
+
+const EXAMPLE_PATH = path.resolve(__dirname, './examples');
+const underDevelopment = () => {
+    console.log(chalk.yellowBright('😢 Under development...'));
+};
+const dynamicCommands: Partial<any> = {
+    exampleList: safeRequire<any>('@brainly-api/example-list'),
+    localServer: safeRequire<any>('@brainly-api/local-server'),
+    docsServer: safeRequire<any>('@brainly-api/docs-server')
+};
+
+program
+    .name('brainly-api')
+    .version('0.0.4')
+    .addHelpText('beforeAll', chalk.bold(`${chalk.greenBright('BRAINLY-API')} CLI TOOL`))
+    .configureOutput({
+        outputError: (errorString, write) => write(chalk.redBright(errorString))
+    });
+
+program
+    .command('tool <tooltype>')
+    .description('install required tool, e.g auth, random-useragent')
+    .action(underDevelopment);
+
+for (const commandName in dynamicCommands) {
+    const dynamicCommand: any = dynamicCommands[commandName];
+
+    if (dynamicCommand !== null) {
+        dynamicCommand.register(program);
+    }
+}
+
+program.parse();
