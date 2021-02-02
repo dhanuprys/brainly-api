7bacf94e9c27b19367d86a34dc4f582bacc8251d
dhanuprys <dhanuprys@gmail.com>
2021-02-02 16:28:53 +0800

    Upgrade version to 0.0.39

--- postinstall.js
+++ postinstall.js
@@ -3,11 +3,11 @@ const path = require('path');
 const chalk = require('chalk');
 const rimraf = require('rimraf');
 
-if (env.BUILD_MODE === '1') {
-    console.log(chalk.blueBright('Removing unused files..'));
-    rimraf(path.resolve(__dirname, 'src'), () => {
-        rimraf(path.resolve(__dirname, 'tsconfig.json'), () => {});
-    });
-}
+//if (env.BUILD_MODE === '1') {
+//  console.log(chalk.blueBright('Removing unused files..'));
+//   rimraf(path.resolve(__dirname, 'src'), () => {
+//      rimraf(path.resolve(__dirname, 'tsconfig.json'), () => {});
+//    });
+//}
 
 console.log(chalk.yellowBright('Thanks for using brainly-api :)'));
