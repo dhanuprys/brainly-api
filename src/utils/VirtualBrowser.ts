d3fba67d5392e6a0a3ae26a7631e996d959fac4b
dhanuprys <dhanuprys@gmail.com>
2021-03-29 20:37:18 +0800

    currently disable puppeteer

--- src/utils/VirtualBrowser.ts
+++ src/utils/VirtualBrowser.ts
@@ -1,5 +1,30 @@
+import safeRequire from '../functions/safeRequire';
 class VirtualBrowser {
+    // private _puppeteer: typeof Puppeteer;
+    // private _puppeteerConfig: any;
 
+    constructor(puppeteerConfig: any) {
+        // this._puppeteer = safeRequire('puppeteer', () => {
+        //     throw new Error('You need install puppeteer first');
+        // });
+
+        // this._puppeteerConfig = puppeteerConfig;
+    }
+    launch() {
+        
+    }
+    
+    signin() {
+
+    }
+
+    signup() {
+
+    }
+
+    close() {
+
+    }
 }
 
 export default VirtualBrowser;
