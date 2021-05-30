effc67ba79dedb576feeda0c4474210ae31e7533
dhanuprys <dhanuprys@gmail.com>
2021-05-30 12:01:47 +0800

    Add server port

--- packages/brainly-api-tools/portable-server/index.js
+++ packages/brainly-api-tools/portable-server/index.js
@@ -1,9 +1,10 @@
 function registerProgram(program) {
     program
         .command('portable-server')
+        .option('-p, --port <port>', 'Manually set server port')
         .description('start portable server')
-        .action(() => {
-            console.log('under development');
+        .action((option) => {
+            console.log(option)
         });
 }
 
